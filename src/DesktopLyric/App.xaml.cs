ba0324d6ad168970914309ab98eed3dbf4e98316
Epi-1120 <128920892+Epi-1120@users.noreply.github.com>
2026-04-28 19:47:06 +0800

    single instance check, was opening duplicates

--- src/DesktopLyric/App.xaml.cs
+++ src/DesktopLyric/App.xaml.cs
@@ -1,7 +1,28 @@
+using System.Threading;
 using System.Windows;
 
 namespace DesktopLyric;
 
 public partial class App : Application
 {
+    private static Mutex? _mutex;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _mutex = new Mutex(true, "DesktopLyric_SingleInstance", out bool isNew);
+        if (!isNew)
+        {
+            MessageBox.Show("already running", "Desktop Lyric");
+            Shutdown();
+            return;
+        }
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _mutex?.ReleaseMutex();
+        _mutex?.Dispose();
+        base.OnExit(e);
+    }
 }
